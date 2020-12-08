31007c13019289c4603d82efaa5ed129b617568a
lucas <ljeronimodarocha@gmail.com>
2020-12-08 17:17:10 -0300

    .

---
+++ exe2.java
@@ -0,0 +1,9 @@
+public class exe2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++)
+            System.out.println(i);
+        System.out.println(i);
+        System.out.println("finished");
+
+    }
+}
---
+++ exe5.java
@@ -0,0 +1,9 @@
+public class exe5 {
+    static int i = 3;
+
+    public static void main(String[] a) {
+        for (new exe5().i = 10; new exe5().i < 100; new exe5().i++) {
+            System.out.println(i);
+        }
+    }
+}
